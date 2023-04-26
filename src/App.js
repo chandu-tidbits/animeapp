dfa69f269b07995ad947c7f1e3ef59f99ac24f0e
scupdev <ot.gaming132@gmail.com>
2023-04-26 22:10:51 +0530

    added phantom wallet auth

--- src/App.js
+++ src/App.js
@@ -4,13 +4,24 @@ import React, { useEffect, useState } from "react";
 const TWITTER_LINK = "https://twitter.com/chanlan07";
 const TWITTER_HANDLE = "chanlan07";
 
+const TEST_GIFS = [
+  "https://i.giphy.com/media/eIG0HfouRQJQr1wBzz/giphy.webp",
+  "https://media3.giphy.com/media/L71a8LW2UrKwPaWNYM/giphy.gif?cid=ecf05e47rr9qizx2msjucl1xyvuu47d7kf25tqt2lvo024uo&rid=giphy.gif&ct=g",
+  "https://media4.giphy.com/media/AeFmQjHMtEySooOc8K/giphy.gif?cid=ecf05e47qdzhdma2y3ugn32lkgi972z9mpfzocjj6z1ro4ec&rid=giphy.gif&ct=g",
+  "https://i.giphy.com/media/PAqjdPkJLDsmBRSYUp/giphy.webp",
+];
+
 function App() {
+  const [walletAddress, setWalletAddress] = useState(null);
+
   const checkIfWalletIsConnected = async () => {
     if (window.solana.isPhantom) {
       console.log("Wallet found!");
 
       const res = await window.solana.connect({ onlyIfTrusted: true });
       console.log("Connected with public key: ", res.publicKey.toString());
+
+      setWalletAddress(res.publicKey.toString());
     } else {
       console.log("Install phantom");
       alert("install phantom at phantom.app");
@@ -25,7 +36,15 @@ function App() {
     return () => window.removeEventListener("load", onLoad);
   }, []);
 
-  const connectWallet = async () => {};
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (solana) {
+      const res = await solana.connect();
+      console.log("Connected with public key: ", res.publicKey.toString());
+      setWalletAddress(res.publicKey.toString());
+    }
+  };
 
   const renderNotConnectedContainer = () => (
     <button
@@ -38,13 +57,13 @@ function App() {
 
   return (
     <div className="App">
-      <div className="container">
+      <div className={walletAddress ? "authed-container" : "container"}>
         <div className="header-container">
           <p className="header">🖼 GIF Portal</p>
           <p className="sub-text">
             View your GIF collection in the metaverse ✨
           </p>
-          {renderNotConnectedContainer()}
+          {!walletAddress && renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
           <img
